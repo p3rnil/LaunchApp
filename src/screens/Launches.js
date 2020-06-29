bd3535c108bfdf29f273d8e244afa035b0bd48b7
Nico <nicolasacostap@gmail.com>
2020-06-29 13:04:29 +0200

    Removed Text

--- src/screens/Launches.js
+++ src/screens/Launches.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { LaunchesList } from '../components/index';
 import { LaunchProvider } from '../context/index';
 
